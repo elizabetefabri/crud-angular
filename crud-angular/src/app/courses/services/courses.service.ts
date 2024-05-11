f10c231f4c03f7ebd59bd68ea695e09fb71a1668
Elizabete <elizabete958@gmail.com>
2024-05-11 16:39:00 -0300

    informando ao serviço de onde consumir os dados

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -2,20 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Course } from '../model/course';
+import { first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
+  private readonly API = '/assets/course.json';
 
-  constructor(
-    private httpClient: HttpClient,
+  constructor(private httpClient: HttpClient) { }
 
-  ) { }
-
-  list(): Course[] {
-    return [
-      { _id: '1', name: 'Angular', category: 'front-end'}
-    ];
+  list() {
+    return this.httpClient.get<Course[]>(this.API).pipe(
+      first(),
+      tap(courses => console.log(courses))
+    );
   }
 }
