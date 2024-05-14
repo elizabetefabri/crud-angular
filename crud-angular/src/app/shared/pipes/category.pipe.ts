212bc7968acf9b26aa092350847f89137dde088b
Elizabete <elizabete958@gmail.com>
2024-05-13 22:13:40 -0300

    add pipe com switch case para mostrar o icone

--- crud-angular/src/app/shared/pipes/category.pipe.ts
+++ crud-angular/src/app/shared/pipes/category.pipe.ts
@@ -5,8 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CategoryPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string): string {
+    switch(value) {
+      case 'front-end': return 'code';
+      case 'back-end': return 'computer';
+
+    }
+    return 'code';
   }
 
 }
