76923df8bff81f0d23cf4a27b9cdde9ff8d11b59
Elizabete <elizabete958@gmail.com>
2024-05-13 21:43:31 -0300

    add função de capturar o Error

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -14,11 +16,22 @@ export class CoursesComponent implements OnInit{
   displayedColumns = ['name', 'category'];
 
   constructor(
-    private coursesService: CoursesService
+    private coursesService: CoursesService,
+    public dialog: MatDialog
   ) {
-    this.courses$ = this.coursesService.list();
+    this.courses$ = this.coursesService.list().pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar cursos. 😕')
+        return of([])
+      })
+    );
    }
 
+   onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
   ngOnInit(): void {
 
    }
