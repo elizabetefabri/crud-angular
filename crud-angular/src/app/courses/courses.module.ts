971b48c01984771a3eb05a3d673c482581b772c6
Elizabete <elizabete958@gmail.com>
2024-05-30 18:28:51 -0300

    add metodo para salvar dados novo

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -8,6 +8,7 @@ import { CoursesComponent } from './courses/courses.component';
 import { CourseFormComponent } from './course-form/course-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
+
 @NgModule({
   declarations: [
     CoursesComponent,
